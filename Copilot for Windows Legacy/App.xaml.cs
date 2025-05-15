37bff5cbc329388019195e31da1deb46fac6fd7d
Johnny <Johnny@NOTEBOOK>
2025-05-15 19:21:20 -0300

    Add project files.

---
+++ Copilot for Windows Legacy/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using CefSharp;
+using CefSharp.Wpf;
+using System.Windows;
+
+namespace Copilot_for_Windows_Legacy
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            if (!Cef.IsInitialized)
+            {
+                var settings = new CefSettings();
+                settings.CachePath = @"C:\MeuApp\Cache";
+                Cef.Initialize(settings);
+            }
+        }
+    }
+
+}
---
+++ Copilot for Windows Legacy/CacheManager.cs
@@ -0,0 +1,31 @@
+﻿using CefSharp.Wpf;
+using CefSharp;
+
+namespace Copilot_for_Windows_Legacy
+{
+    class CacheManager
+    {
+        public static void ConfigureCache()
+        {
+            var settings = new CefSettings();
+            settings.CachePath = @"C:\MeuApp\Cache";
+            settings.BrowserSubprocessPath = "CefSharp.BrowserSubprocess.exe";
+            Cef.Initialize(settings);
+            Log();
+
+
+            if (!Cef.IsInitialized)
+            {
+                Cef.Initialize(settings);
+            }
+        }
+
+        public static void Log()
+        {
+            var settings = new CefSettings();
+            settings.LogSeverity = LogSeverity.Info;
+            settings.LogFile = @"C:\MeuApp\cef_log.txt";
+
+        }
+    }
+}
