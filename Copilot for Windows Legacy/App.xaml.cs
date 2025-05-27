00c7e2d76dc639aebeff0a3645f967e3583c2def
Johnny Jeronimo <jjj-042012@hotmail.com>
2025-05-26 22:07:50 -0300

    App.xaml.cs update

--- Copilot for Windows Legacy/App.xaml.cs
+++ Copilot for Windows Legacy/App.xaml.cs
@@ -3,11 +3,13 @@ using CefSharp.Wpf;
 using System;
 using System.IO;
 using System.Windows;
+using System.Threading;
 
 namespace Copilot_for_Windows_Legacy
 {
     public partial class App : Application
     {
+        private static Mutex mutex;
         protected override void OnStartup(StartupEventArgs e)
         { 
             var settings = new CefSettings();
@@ -31,7 +33,30 @@ namespace Copilot_for_Windows_Legacy
                 Cef.Initialize(settings);
             }
             base.OnStartup(e);
+
+           // SingleInstance();
         }
+
+
+        //This still doesn't work
+        public void SingleInstance()
+        {
+            const string appName = "Copilot for Windows Legacy";
+            bool createdNew;
+
+            mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("The application is already running.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    
 
         //Shutdown CEF
         protected override void OnExit(ExitEventArgs e)
